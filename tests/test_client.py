4c113c97cc481d7b00ff9475b37ecd8a43d2e604
Michael Cooper <macoop2363@gmail.com>
2021-01-29 12:29:06 -0700

    Add test to get objects from s3

--- tests/test_client.py
+++ tests/test_client.py
@@ -16,11 +16,11 @@ def bucket_name():
 
 @pytest.fixture()
 def s3_bucket_with_objects(s3_client, bucket_name):
-    s3_client.create_bucket(Bucket=bucket_name)
-    s3_client.put_object(Body=b'hello', Bucket=bucket_name, Key='prefix_1/hello.csv')
-    s3_client.put_object(Body=b'world', Bucket=bucket_name, Key='prefix_1/world.csv')
-    s3_client.put_object(Body=b'hello', Bucket=bucket_name, Key='prefix_2/hello.csv')
-    yield
+    bucket = s3_client.create_bucket(Bucket=bucket_name)
+    bucket.put_object(Body=b'hello', Key='prefix_1/hello.csv')
+    bucket.put_object(Body=b'world', Key='prefix_1/world.csv')
+    bucket.put_object(Body=b'hello', Key='prefix_2/hello.csv')
+    yield s3_client
 
 @pytest.fixture()
 def client():
@@ -30,10 +30,17 @@ def test_it_creates_a_valid_session(client):
     assert type(client._session) == boto3.Session
     assert client._s3 is not None
 
-@mock_s3
 def test_get_bucket(bucket_name, client):
     bucket = client.get_bucket(bucket_name)
     assert bucket.name == bucket_name
+
+def test_get_objects(bucket_name, s3_bucket_with_objects, client):
+    bucket = client.get_bucket(bucket_name)
+    objects = client.get_objects(bucket)
+    assert list(objects) == list(s3_bucket_with_objects.Bucket(bucket_name).objects.all())
+
+    filtered_objects = client.get_objects(bucket, 'prefix_1')
+    assert list(filtered_objects) == list(s3_bucket_with_objects.Bucket(bucket_name).objects.filter(Prefix='prefix_1'))
 
 def test_get_schema(client):
     schema = client.get_schema(
